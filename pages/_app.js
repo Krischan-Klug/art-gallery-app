ba6f219c37b350fd2d4bdf57ef975b5bfcb89718
Henrik <henrik@bischkopf.de>
2024-03-11 11:48:33 +0100

    switched Fetch logic to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
 
 export default function App({ Component, pageProps }) {
+  const URL = "https://example-apis.vercel.app/api/art";
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(URL, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} pieces={data} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,10 @@
 import ArtPieces from "@/components/ArtPieces";
-import useSWR from "swr";
 
-export default function HomePage() {
-  const URL = "https://example-apis.vercel.app/api/art";
-
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-  const { data, error, isLoading } = useSWR(URL, fetcher);
-
-  if (error) return <div>failed to load</div>;
-  if (isLoading) return <div>loading...</div>;
-
+export default function HomePage({ pieces }) {
   return (
     <div>
       <h1>Art Pieces</h1>
-      <ArtPieces pieces={data}></ArtPieces>
+      <ArtPieces pieces={pieces}></ArtPieces>
     </div>
   );
 }
