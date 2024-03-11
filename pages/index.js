89e87a47162880876ec8715c3993bc194ff1b42e
Krischan-Klug <krischanklug@icloud.com>
2024-03-11 11:29:52 +0100

    finished task 1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import ArtPieces from "@/components/ArtPieces";
+import useSWR from "swr";
+
 export default function HomePage() {
+  const URL = "https://example-apis.vercel.app/api/art";
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(URL, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Art Pieces</h1>
+      <ArtPieces pieces={data}></ArtPieces>
     </div>
   );
 }
