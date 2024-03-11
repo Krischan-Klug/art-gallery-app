f16730ef7580a9c3a583f5e68b9c82b5ca8c0d8e
Henrik <henrik@bischkopf.de>
2024-03-11 12:14:10 +0100

    solved UserStory2

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import ArtPieces from "@/components/ArtPieces";
+import Spotlight from "@/components/Spotlight";
 
 export default function HomePage({ pieces }) {
   return (
     <div>
+      <h1>Random Art Piece</h1>
+      <Spotlight pieces={pieces}></Spotlight>
       <h1>Art Pieces</h1>
       <ArtPieces pieces={pieces}></ArtPieces>
     </div>
